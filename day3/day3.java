aa7a7fbecaf2b67fedeca17cc7548b3042bac50d
almathaler <athaler00@stuy.edu>
2019-07-16 17:53:52 +0300

    part2

--- day3/day3.java
+++ day3/day3.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
 public class day3{
   public static void main(String[] args){
     try{
@@ -9,7 +10,7 @@ public class day3{
       System.out.println(e);
     }
   }
-  public static int getOverlap(String filename) throws FileNotFoundException{
+  public static String getOverlap(String filename) throws FileNotFoundException{
     File f = new File(filename);
     Scanner inputs = new Scanner(f);
     int[][] grid = new int[1000][1000]; //all spaces are -1. if first, make 1. if 2nd, make 2. add everything up and / by 2
@@ -39,15 +40,35 @@ public class day3{
         }
       }
     }
-    int sum = 0;
-    for (int c = 0; c<1000; c++){
-      for (int d = 0; d<1000; d++){
-        if (grid[c][d] == 2){
-          sum+=grid[c][d];
+    inputs.close();
+    Scanner inputsS = new Scanner(f);
+    while (inputsS.hasNext()){
+      boolean theOne = true;
+      String claim = inputsS.nextLine().trim(); //(393,863: 11x29)
+      String toReturn = claim;
+      claim = claim.substring(claim.indexOf('@') + 2);
+      claim = claim.replace(": ", ",");
+      claim = claim.replace("x", ","); //(393,863,11,29)
+      String[] instructions = claim.split(","); // {393, 863, 11, 29}
+      int startF = Integer.parseInt(instructions[1]); //bc the first num is space from left, so that's index in 2nd array
+      int startS = Integer.parseInt(instructions[0]);
+      int width = Integer.parseInt(instructions[2]);
+      int height = Integer.parseInt(instructions[3]);
+      for (int i = 0; i<height; i++){
+        for (int j = 0; j<width; j++){
+          if (grid[startF+i][startS+j] != 1){
+            theOne = false;
+            j = width;
+            i = height;
+            //System.out.println(toReturn + ", NO");
+          }
         }
       }
+      if (theOne){
+        return toReturn;
+      }
     }
-    sum/=2;
-    return sum;
+    return "none";
+
   }
 }
